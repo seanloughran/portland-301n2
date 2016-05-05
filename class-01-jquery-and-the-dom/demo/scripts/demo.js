f12bcee259bdac046736ef85a7a6967643fb4919
Ken Brumer <kbrumer@sosacorp.com>
2016-05-04 17:48:10 -0700

    Updating demo.

--- class-01-jquery-and-the-dom/demo/scripts/demo.js
+++ class-01-jquery-and-the-dom/demo/scripts/demo.js
@@ -1,1 +1,14 @@
 
+$(function(){
+  $('#my-template').find('li').html('foozie');
+
+
+
+  //var $myTemplate = $('#my-template').clone();
+  //$myTemplate.append('<hr>');
+  //$myTemplate.find('h1').html('New improved');
+  //$myTemplate.find('li:first').html('I like this class.');
+  //$myTemplate.find('li:last a').attr('href', 'https://twitter.com/kbrumer');
+  //$('main').append('<hr>').append($myTemplate);
+  //$('#my-template').fadeOut();
+});
