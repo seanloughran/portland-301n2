8a543b593e478746c18addaed2b7631871eb56bc
Keenan <k.p.wellsy@gmail.com>
2016-05-09 16:27:48 -0700

    updated page with nav control and articleView.js file. Added events to view for displaying about or projects section, and filtering projects by category

--- portfolio-assignment/pauwlsky-portfolio/js/projectViews.js
+++ portfolio-assignment/pauwlsky-portfolio/js/projectViews.js
@@ -2,19 +2,25 @@ portfolioView = {};
 
 portfolioView.populateFilters=function(){
   $.ajax({
-    dataType: "json",
-    url:"js/portfolioitems.json",
+    dataType: 'json',
+    url:'js/portfolioitems.json',
     success:function(data){
       data.forEach(function(item){
-        console.log(item.title);
-        var option = '<option val="' + item.title + '">' + item.title + '</option>';
-        $('#skills-select').append(option);
+        var skills = item.skills;
+        skills.forEach(function(item){
+          var option = '<option val="' + item + '">' + item + '</option>';
+          if($('#skills-select option[val="'+ item +'"]').length===0){
+            console.log(($('#skills-select option[val="'+ item +'"]').length));
+            $('#skills-select').append(option);
+          }
+        });
       });
     }
   });
 };
 
-portfolioView.filterSelected = function(){
+portfolioView.showSection = function(){
+  $('#about').hide();
   $('#project-li').on('click', function(e){
     e.preventDefault();
     $('#about').hide();
@@ -27,16 +33,26 @@ portfolioView.filterSelected = function(){
   });
 };
 
-portfolioView.showSection = function(){
-
+portfolioView.filterSelected = function(){
+  $('#skills-select').on('change', function(e){
+    var $selectVal = $(this).val();
+    $projectCategories = $('a[data-category="'+ $selectVal +'"]');
+    $('article').hide();
+    $projectCategories.parents('article').fadeIn();
+  });
 };
 
 portfolioView.showMore = function(){
-
+  $('.text').find('p:gt(0)').hide();
+  $('article').on('click', '.show-more', function(e){
+    e.preventDefault();
+    $(this).prev().children('p').fadeIn();
+  });
 };
 
 $(function(){
-  portfolioView.filterSelected();
   portfolioView.populateFilters();
-
+  portfolioView.showSection();
+  portfolioView.filterSelected();
+  portfolioView.showMore();
 });
