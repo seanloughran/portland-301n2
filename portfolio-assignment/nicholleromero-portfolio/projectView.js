cec5961fdb976e9eeccabdd10287e374a5c75f55
nicholleromero <nicholleromero@gmail.com>
2016-05-18 10:52:55 -0700

    Updating portfolio (#46)

--- portfolio-assignment/nicholleromero-portfolio/projectView.js
+++ portfolio-assignment/nicholleromero-portfolio/projectView.js
@@ -1,26 +1,20 @@
 var projectView = {};
 
 projectView.handleNav = function() {
-  console.log('handleNav runs');
-  $('.navigation .tab').on('click', function() {
+  $('.navigation li.tab').on('click', function() {
       var tab_id = $(this).attr('data-content');
       var selectedTab = $('#'+tab_id);
 
-
       $('.tab-content').hide();
       selectedTab.show();
-      console.log('tab function runs');
   });
 
   $('.navigation .tab:first').click();
-
 };
 
-
-$('document').ready(function(){
-projectView.handleNav();
-  console.log('document loads')
-});
+$(document).ready(function(){
+  projectView.handleNav();
+})
 // $(function(){
 //   $('address').each(function(){
 //     var val = $(this).find('address a').text();
