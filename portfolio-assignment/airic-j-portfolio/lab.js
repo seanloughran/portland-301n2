79ec20cbf68ba7faabb79a3053e6dde0ae700879
Eric Johnson <eric+github@ericslab.com>
2016-05-19 11:19:25 -0700

    Move blog into CodeFellows repo (old) add Handlebars, SMACCS and AJAX (new) (#28)
    * move project into codefellows-301 repo
    
    * add images to remote repo
    
    * add handlebars CDN to index.html
    
    * add handlebars
    
    * update css to follow smaccs
    
    * move project link into title
    
    * update font styles
    
    * add weather AJAX call for current temp

--- portfolio-assignment/airic-j-portfolio/lab.js
+++ portfolio-assignment/airic-j-portfolio/lab.js
@@ -14,17 +14,11 @@ $('document').ready(function () {
   }
 
   Project.prototype.toHtml = function() {
-    var $newProject = $('.prototype').clone();
-    $newProject.find('#title').html(this.title);
-    $newProject.find('#date').html(this.date);
-    $newProject.find('#link a').attr('href',this.link);
-    // $newProject.find('#link a').html(this.link);
-    $newProject.find('#description').html(this.description);
-    // TODO get this working
-    $newProject.find('#projectImage').attr('src',this.image);
-
-    $newProject.removeClass('prototype');
-    return $newProject;
+    // use handlebars
+    var templateScript = $('#projectTemplate').html();
+    var projectTemplate = Handlebars.compile(templateScript);
+    var html = projectTemplate(this);
+    return(html);
   };
 
   projectData.forEach(function(projectData){
@@ -52,6 +46,14 @@ $('document').ready(function () {
   $(window).scroll(function() {
     stickyNav();
   });
+
+  // connect page weather conditions
+  var weatherAPI = 'http://api.wunderground.com/api/c57bffbbb79db788/geolookup/conditions/q/OR/Portland.json';
+  var successFunction = function(data) {
+    $('.weather').append((' where it is ' + Math.round(data.current_observation.temp_f) + '&deg; F') + (' and ' + data.current_observation.weather).toLowerCase());
+    console.log(data.current_observation.temp_f);
+  };//end of weather callback
+  $.getJSON(weatherAPI, successFunction);
 
 
 	// change visible tab by click on nav links
