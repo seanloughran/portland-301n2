9613fe000d972a5eac82c5b9fbc24adad5bd39b5
seanloughran <seancloughran@gmail.com>
2016-05-23 21:16:01 -0700

    Portfolio changing data to json (#67)
    * better looking page in general
    
    * Changes template function to use handlebars
    
    * Adds third project to main page
    
    * Adds typography, shifts css into created base and state css files and deletes original css file.
    
    * completed assignment for the class-10 ajax lab
    
    * Adds ajax request
    
    * Changes Data to JSON
    
    * Changes trying to find issue with ajax
    
    * Project now works using json
    
    * Adds read more/read less function to project descriptions

---
+++ portfolio-assignment/seanloughran-portfolio/JS/pageFunction.js
@@ -0,0 +1,42 @@
+var projectView = {};
+
+//Functionality for home and about me tabs.
+projectView.tabNavigation = function() {
+  $('nav').on('click','li',function(){
+    $('.tab-content').hide();
+    $('body').find('[id="'+$(this).attr('data-content')+'"]').show();
+  });
+
+  $('nav .tab:first').click();
+};
+
+projectView.showMoreDescription = function() {
+  $('.project_description *:nth-of-type(n+2)').hide();
+
+  $('#projects').on('click', '.read-more', function(e) {
+    e.preventDefault();
+    $(this).parent().find('p:nth-of-type(n+2)').fadeToggle();
+
+    var html = $('.read-more').html();
+    console.log(html);
+
+    if (html == 'Read More') {
+      $('.read-more').html('Read Less');
+      console.log('Works');
+    } else {
+      $('.read-more').html('Read More');
+    }
+  });
+
+};
+
+
+projectView.initPrimaryPage = function(){
+  //Goes through project Array and appends them to the 'main' element
+  Project.all.forEach(function(a){
+    $('main').append(a.addProject(a));
+  });
+
+  projectView.tabNavigation();
+  projectView.showMoreDescription();
+};
