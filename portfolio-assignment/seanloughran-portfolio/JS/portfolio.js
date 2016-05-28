e91e3a6c75ed2621737167b3991698f5f8c9386d
seanloughran <seancloughran@gmail.com>
2016-05-28 14:15:33 -0700

    Portfolio func prog (#83)
    * better looking page in general
    
    * Changes template function to use handlebars
    
    * Adds third project to main page
    
    * Adds typography, shifts css into created base and state css files and deletes original css file.
    
    * completed assignment for the class-10 ajax lab
    
    * Adds ajax request
    
    * Changes Data to JSON
    
    * Changes trying to find issue with ajax
    
    * Project now works using json
    
    * Adds read more/read less function to project descriptions
    
    * changes read more style
    
    * This added show more functionality
    
    * adds IFFE
    
    * miscellaneous
    
    * Changes localLoad function to use map() programming

--- portfolio-assignment/seanloughran-portfolio/JS/portfolio.js
+++ portfolio-assignment/seanloughran-portfolio/JS/portfolio.js
@@ -1,55 +1,61 @@
+(function(module) {
 
-//project object constructor
-function Project(opts) {
-  this.title = opts.title;
-  this.ghPagesUrl = opts.ghPagesUrl;
-  this.linkTitle = opts.linkTitle;
-  this.image = opts.image;
-  this.description = opts.description;
-}
+  //project object constructor
+  function Project(opts) {
+    this.title = opts.title;
+    this.ghPagesUrl = opts.ghPagesUrl;
+    this.linkTitle = opts.linkTitle;
+    this.image = opts.image;
+    this.description = opts.description;
+  }
 
-Project.all = [];
+  Project.all = [];
 
-//Clones project template from page, adds in information, and returns that grouped together.
-Project.prototype.addProject = function() {
-  var templateScript = $('#projectTemplate').html();
-  var compiledTemplate = Handlebars.compile(templateScript);
+  //Clones project template from page, adds in information, and returns that grouped together.
+  Project.prototype.addProject = function() {
+    var templateScript = $('#projectTemplate').html();
+    var compiledTemplate = Handlebars.compile(templateScript);
 
-  return compiledTemplate(this);
-  console.log('Project added');
+    return compiledTemplate(this);
+    console.log('Handlebars template compiled');
 
-};
+  };
 
 
-//Goes throuh projectData info and pushes to the articles array at the top of this page.
-Project.localLoad = function(localSData) {
-  localSData.forEach(function(projectRawObject) {
-    Project.all.push(new Project(projectRawObject));
-  });
-};
+  //Goes through projectData info and pushes to the Project.all array.
+  Project.localLoad = function(localSData) {
+    // localSData.forEach(function(projectRawObject) {
+    //   Project.all.push(new Project(projectRawObject));
+    // });
+    Project.all = localSData.map(function(projectRawObject) {
+      console.log('Projects mapped');
+      return new Project(projectRawObject);
+    });
+  };
 
-Project.infoFetch = function() {
-  if (localStorage.localSData) {
+  Project.infoFetch = function() {
+    if (localStorage.localSData) {
 
-    Project.localLoad(JSON.parse(localStorage.getItem('localSData')));
-    console.log('Local storage fetched.');
-    projectView.initPrimaryPage();
+      Project.localLoad(JSON.parse(localStorage.getItem('localSData')));
+      console.log('Local storage fetched.');
+      projectView.initPrimaryPage();
 
-  } else {
-    $.ajax({
-      type: 'GET',
-      url: 'Data/projectData.json',
+    } else {
+      $.ajax({
+        type: 'GET',
+        url: 'Data/projectData.json',
         success: function(data) {
           Project.localLoad(data);
           localStorage.setItem('localSData', JSON.stringify(data));
           projectView.initPrimaryPage();
           console.log(data);
         }
-    });
-    console.log('local storage created');
-  }
-};
+      });
+      console.log('local storage created');
+    }
+  };
 
 
-
-//$('.template').hide();
+  //$('.template').hide();
+  module.Project = Project;
+})(window);
