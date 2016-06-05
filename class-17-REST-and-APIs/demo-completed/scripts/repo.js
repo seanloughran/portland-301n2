cea6b273cc70175b19c06f81de9ea9d28136cb3c
Ken Brumer <kbrumer@sosacorp.com>
2016-06-05 08:58:25 -0700

    Finishing up demo.

--- class-17-REST-and-APIs/demo-completed/scripts/repo.js
+++ class-17-REST-and-APIs/demo-completed/scripts/repo.js
@@ -3,7 +3,6 @@
 
   repos.all = [];
 
-  // TODO: How would you like to fetch your repos? Don't forget to call the callback.
   repos.requestRepos = function(callback) {
     $.ajax({
       url: 'https://api.github.com/users/kbrumer/repos' +
@@ -14,16 +13,7 @@
       success: function(data, message, xhr) {
         repos.all = data;
       }
-    }).done(callback);
-  };
-
-
-  // DONE: Model method that filters the full collection for repos with a particular attribute.
-  // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
-  repos.with = function(attr) {
-    return repos.all.filter(function(repo) {
-      return repo[attr];
-    });
+    }).then(callback);
   };
 
   module.repos = repos;
