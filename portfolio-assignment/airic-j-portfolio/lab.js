12d3a951f7d27ec84ed027b0e774b9f494f93036
Eric Johnson <eric+github@ericslab.com>
2016-06-06 15:14:11 -0700

    Class 09 refactor (#102)
    * move project into codefellows-301 repo
    
    * add images to remote repo
    
    * add handlebars CDN to index.html
    
    * add handlebars
    
    * update css to follow smaccs
    
    * move project link into title
    
    * update font styles
    
    * add weather AJAX call for current temp
    
    * refactor to add iffe and more FP

--- portfolio-assignment/airic-j-portfolio/lab.js
+++ portfolio-assignment/airic-j-portfolio/lab.js
@@ -1,9 +1,8 @@
-$('document').ready(function () {
+// TODO assignment 09 - make this a real iffe
+// $('document').ready(function () {
+(function(module){
 
-	// hide projects at initial page load
-  $('section').not('#about').hide();
-
-// project constructor function
+  // project constructor function
   function Project(x) {
     this.title = x.title;
     this.date = x.date;
@@ -11,6 +10,63 @@ $('document').ready(function () {
     this.link = x.link;
     this.image = x.image;
   }
+
+  var ProjectsModule = {
+
+    hideAbout: function() {
+      $('section').not('#about').hide();
+    },
+
+    // todo assignment 09
+    toggleStickyNav: function(topOfNav) {
+      var scrollTop = $(window).scrollTop();
+
+      if (scrollTop >= topOfNav && scrollTop > 0) {
+        $('nav').addClass('stickyNav').removeClass('nav');
+      } else {
+        $('nav').removeClass('stickyNav').addClass('nav');
+      }
+    },
+
+    stickyNav: function() {
+      var topOfNav = $('nav').offset().top;
+      this.toggleStickyNav(topOfNav);
+    },
+
+    navClick: function() {
+      // change visible tab by click on nav links
+      $('nav a').on('click', function() {
+        $tabClicked = $(this).data('tab');
+        $(this).addClass('selected').siblings().removeClass('selected');
+        $('section').hide();
+        $('#' + $tabClicked).show();
+      });
+    },
+
+    getWeather: function() {
+      // connect page weather conditions
+      var weatherAPI = 'http://api.wunderground.com/api/c57bffbbb79db788/geolookup/conditions/q/OR/Portland.json';
+      var successFunction = function(data) {
+        $('.weather').append((' where it is ' + Math.round(data.current_observation.temp_f) + '&deg; F') + (' and ' + data.current_observation.weather).toLowerCase());
+      };//end of weather callback
+      $.getJSON(weatherAPI, successFunction);
+    },
+
+    projectJSON: function() {
+      // ajax call for project data, map to array and append as HTML to page
+      $.getJSON('data/projectData.json', function(projectJSON){
+        // put each JSON element into the projects array after making it a Project object
+        console.log(projectJSON);
+        projectsArray = projectJSON.map(function(project){
+          return (new Project(project));
+        }).forEach(function(project){
+          $('#projects').append(project.toHtml());
+        });
+      });
+    }
+
+  };
+
 
 // Project function method for project to HTML
   Project.prototype.toHtml = function() {
@@ -21,46 +77,20 @@ $('document').ready(function () {
     return(html);
   };
 
-// ajax call for project data, map to array and append as HTML to page
-  $.getJSON('data/projectData.json', function(projectJSON){
-    // put each JSON element into the projects array after making it a Project object
-    console.log(projectJSON);
-    projectsArray = projectJSON.map(function(project){
-      return (new Project(project));
-    }).forEach(function(project){
-      $('#projects').append(project.toHtml());
-    });
-  });
-
-	//sticky nav
-  var topOfNav = $('.nav').offset().top;
-  function stickyNav() {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > topOfNav) {
-      $('nav').addClass('stickyNav').removeClass('nav');
-    } else {
-      $('nav').removeClass('stickyNav').addClass('nav');
-    }
+  ProjectsModule.init = function() {
+    ProjectsModule.hideAbout();
+    ProjectsModule.navClick();
+    ProjectsModule.getWeather();
+    ProjectsModule.projectJSON();
   };
 
   $(window).scroll(function() {
-    stickyNav();
+    ProjectsModule.stickyNav();
   });
 
-  // connect page weather conditions
-  var weatherAPI = 'http://api.wunderground.com/api/c57bffbbb79db788/geolookup/conditions/q/OR/Portland.json';
-  var successFunction = function(data) {
-    $('.weather').append((' where it is ' + Math.round(data.current_observation.temp_f) + '&deg; F') + (' and ' + data.current_observation.weather).toLowerCase());
-  };//end of weather callback
-  $.getJSON(weatherAPI, successFunction);
+  module.ProjectsModule = ProjectsModule;
+
+// }); // end document.ready
 
 
-	// change visible tab by click on nav links
-  $('nav a').on('click', function() {
-    $tabClicked = $(this).data('tab');
-    $(this).addClass('selected').siblings().removeClass('selected');
-    $('section').hide();
-    $('#' + $tabClicked).show();
-  });
-
-}); // end iffe
+})(window);
