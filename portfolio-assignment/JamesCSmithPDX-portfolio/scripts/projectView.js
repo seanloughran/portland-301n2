cb46ec3645c2a01fffa1150e98bf0460e3414c4a
James C. Smith <james.c.smithpdx@gmail.com>
2016-06-06 15:08:09 -0700

    Class 16 (#93)
    * added saved index
    
    * added saved css files
    
    * added saved script files
    
    * added saved imgs
    
    * most recent saved index
    
    * SMAACS chages, changed nav button, changed img hover and fixed link
    
    * implemented bootstrap
    
    * Fixed teaser funtion
    
    * Fixed nav button style
    
    * rounded edges of all sections
    
    * changed navbar style
    
    * Added TrajectoryDS project to portfolio
    
    * fixed teaser function
    
    * changed project urls
    
    * fixed image responsiveness
    
    * removed skeleton css
    
    * SMAACs and changed onload style
    
    * changes to html, projectView
    
    * cleaned up merge conflicts
    
    * exploring match height plugin
    
    * IIFE and functional programming
    
    * completed anagram functtion and added results to footer
    
    * adjusted name scramble css
    
    * Resized images; added new project
    
    * refactored anagram to contoller, added on click call of anagram function
    
    * refactored anagram to make own IIFE module
    
    * slight anagram change

--- portfolio-assignment/JamesCSmithPDX-portfolio/scripts/projectView.js
+++ portfolio-assignment/JamesCSmithPDX-portfolio/scripts/projectView.js
@@ -7,6 +7,8 @@
     $('.mainNav').on('click', 'li', function() {
       $('.jumbotron').fadeTo(5000, 1);
       $('.page-content').hide();
+      $('#hbAnagram').remove();
+      anagram.create();
       $('main').find('[id="'+$(this).attr('data-section')+'"]').fadeIn(5000);
       projectView.scroll(this);
     });
@@ -38,60 +40,15 @@
     }, 2000);
   };
 
-  var anagram = {};
-
-  anagram.create = function() {
-    var name = 'JAMES SMITH';  //scramble my name
-    name = name.split(''); // split name into letters
-    console.log(name);
-    anagram.fisherYates(name); //run the ranomd shuffle function
-    console.log(name);
-    // use reduce to join the random letters
-    var myAnagram = name.reduce(function(prev, next, index) {
-      return prev + next;
-    });
-    console.log(myAnagram);
-
-    //get anagram template
-
-    var source = $('#anagram-template').html();
-
-    //compile the anagram
-    var template = Handlebars.compile(source);
-    console.log(myAnagram);
-    var context = {
-      hbAnagram: myAnagram
-    };
-    $('#anagram').append(template(context));
-  };
-
-//scrambled name function with reduce
-
-  anagram.fisherYates = function(name) {
-    var i = name.length, j, tempi, tempj;
-    if ( i === 0 ) return false;
-    while ( --i ) {
-      j = Math.floor( Math.random() * ( i + 1 ) );
-      tempi = name[i];
-      tempj = name[j];
-      name[i] = tempj;
-      name[j] = tempi;
-      console.log('fisheryates:' + name);
-      return name;
-    }
-  };
-
   //call the functions
   projectView.initIndexPage = function(){
     Project.all.forEach(function(a){
       $('#projects').append(a.toHtml());
     });
-    anagram.create();
     $('.page-content').hide();
     $('.jumbotron').fadeTo(15000, 0.0);
     projectView.handleMainNav();
     projectView.createTeaser();
-
   };
   module.projectView = projectView;
 })(window);
